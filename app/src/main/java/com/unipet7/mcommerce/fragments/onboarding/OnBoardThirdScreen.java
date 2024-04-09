4a7c20556e53c020c4861a2e5a6e911a5aea52dd
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-04-10 00:51:57 +0700

    Func: Navigate from third to SignIn

--- app/src/main/java/com/unipet7/mcommerce/fragments/onboarding/OnBoardThirdScreen.java
+++ app/src/main/java/com/unipet7/mcommerce/fragments/onboarding/OnBoardThirdScreen.java
@@ -1,6 +1,8 @@
 package com.unipet7.mcommerce.fragments.onboarding;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +10,23 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.unipet7.mcommerce.R;
+import com.unipet7.mcommerce.activities.SignIn;
+import com.unipet7.mcommerce.databinding.FragmentOnBoardThirdScreenBinding;
 
 public class OnBoardThirdScreen extends Fragment {
+
+    FragmentOnBoardThirdScreenBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_board_third_screen, container, false);
+        binding = FragmentOnBoardThirdScreenBinding.inflate(inflater, container, false);
+
+        binding.btnIntroThirdScreen.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), SignIn.class);
+            startActivity(intent);
+            Log.d("OnBoardThirdScreen", "Bắt đầu thôi đã được nhấn");
+        });
+
+        return binding.getRoot();
     }
 }
