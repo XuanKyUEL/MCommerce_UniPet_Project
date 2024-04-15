f80fd647b799c1c7f3868365dee501f325b86ac1
Luan Nguyen <luannpd2k3@gmail.com>
2024-04-15 22:58:40 +0700

    voucher page

---
+++ app/src/main/java/com/unipet7/mcommerce/VoucherActivity.java
@@ -0,0 +1,14 @@
+package com.unipet7.mcommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoucherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voucher);
+    }
+}
