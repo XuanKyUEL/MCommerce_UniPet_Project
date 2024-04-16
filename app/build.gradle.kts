777d9aa53bceba7070e78b868f69c2619ab31df8
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-04-16 23:02:27 +0700

    Func: save loged status

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,4 +105,5 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
 }
