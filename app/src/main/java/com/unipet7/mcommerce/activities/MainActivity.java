7fd91d94001165164221b5084bc1325274cdf12f
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-04-18 03:41:28 +0700

    UI: Complete bottom navigation

--- app/src/main/java/com/unipet7/mcommerce/activities/MainActivity.java
+++ app/src/main/java/com/unipet7/mcommerce/activities/MainActivity.java
@@ -1,17 +1,25 @@
 package com.unipet7.mcommerce.activities;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.unipet7.mcommerce.R;
 import com.unipet7.mcommerce.databinding.ActivityMainBinding;
+import com.unipet7.mcommerce.fragments.Fragment_Empty_Notification;
 import com.unipet7.mcommerce.fragments.Home;
+import com.unipet7.mcommerce.fragments.Profile;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton fabCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        mapping();
         replaceFragment(new Home());
-        binding.bottomNavigationView.setBackground(null);
+        navigateFragment();
     }
+
+    private void mapping() {
+        bottomNavigationView = binding.bottomNavigationView;
+        fabCart = binding.fabCart;
+    }
+    
+
+    private void navigateFragment() {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            if (item.getItemId() == R.id.home_icon_bottom)
+                fragment = new Home();
+            else if (item.getItemId() == R.id.account_icon_bottom)
+                fragment = new Profile();
+            else if (item.getItemId() == R.id.fav_icon_bottom)
+                fragment = new Fragment_Empty_Notification();
+            else if (item.getItemId() == R.id.product_icon_bottom)
+                fragment = new Fragment_Empty_Notification();
+            replaceFragment(fragment);
+            return true;
+        });
+        fabCart.setOnClickListener(v -> Toast.makeText(this, "FAB clicked", Toast.LENGTH_SHORT).show());
+    }
+
     private void replaceFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
 }
---
+++ app/src/main/java/com/unipet7/mcommerce/fragments/Profile.java
@@ -0,0 +1,38 @@
+package com.unipet7.mcommerce.fragments;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TextAppearanceSpan;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.unipet7.mcommerce.R;
+import com.unipet7.mcommerce.databinding.FragmentProfileBinding;
+
+public class Profile extends Fragment {
+    FragmentProfileBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
