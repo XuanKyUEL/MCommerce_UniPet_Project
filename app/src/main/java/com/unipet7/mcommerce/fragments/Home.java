7535b50bec739b93ca1500d8b59b1042c54609b0
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-04-21 18:19:30 +0700

    Func: Load sales and food for dog and cat

--- app/src/main/java/com/unipet7/mcommerce/fragments/Home.java
+++ app/src/main/java/com/unipet7/mcommerce/fragments/Home.java
@@ -12,7 +12,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.unipet7.mcommerce.R;
 import com.unipet7.mcommerce.adapters.BlogAdapter;
 import com.unipet7.mcommerce.adapters.ProductAdapter;
@@ -24,6 +26,7 @@ import com.unipet7.mcommerce.models.User;
 import com.unipet7.mcommerce.utils.LoadingDialog;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,19 +39,32 @@ public class Home extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    public ProductAdapter adapter;
+
+    public ProductAdapter saleAdapter;
+    public ProductAdapter productDog;
+    public ProductAdapter productCat;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     FragmentHomeBinding binding = null;
-    ProductAdapter adapter;
-    ArrayList<Product> products;
     BlogAdapter blogAdapter;
 
     LoadingDialog loadingDialog;
 
     private static final String KEY_FLAG = "isLoadUser";
     boolean isLoadUser = false;
+
+    public ArrayList<Product> products = new ArrayList<>();
+
+    public ArrayList<Product> productsSale = new ArrayList<>();
+    public ArrayList<Product> productsDog = new ArrayList<>();
+    public ArrayList<Product> productsCat = new ArrayList<>();
+
+    public ProductAdapter adapterSale;
+    public ProductAdapter adapterDog;
+    public ProductAdapter adapterCat;
 
     public Home() {
         // Required empty public constructor
@@ -98,15 +114,37 @@ public class Home extends Fragment {
         if (binding == null) {
             binding = FragmentHomeBinding.inflate(inflater, container, false);
         }
-        initData();
-        loadData();
         loadBlog();
         addEvents();
+        loadHomeUserAndProduct();
+
+        return binding.getRoot();
+    }
+
+    public void configAdapters() {
+
+        saleAdapter = new ProductAdapter(productsSale);
+        binding.lvlHomeSale.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        binding.lvlHomeSale.setAdapter(saleAdapter);
+        binding.lvlHomeSale.setHasFixedSize(true);
+
+        productDog = new ProductAdapter(productsDog);
+        binding.lvlHomeProduct1.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        binding.lvlHomeProduct1.setAdapter(productDog);
+        binding.lvlHomeProduct1.setHasFixedSize(true);
+
+        productCat = new ProductAdapter(productsCat);
+        binding.lvlHomeProduct2.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        binding.lvlHomeProduct2.setAdapter(productCat);
+        binding.lvlHomeProduct2.setHasFixedSize(true);
+    }
+
+    private void loadHomeUserAndProduct() {
         loadingDialog = new LoadingDialog();
         loadingDialog.showLoadingDialog(getContext());
         FireStoreClass fireStoreClass = new FireStoreClass();
+        fireStoreClass.getProductList(this, productsSale, productsDog, productsCat);
         fireStoreClass.loadLoggedUserUI(this);
-        return binding.getRoot();
     }
 
     private void addEvents() {
@@ -141,27 +179,6 @@ public class Home extends Fragment {
         blogs.add(new Blogs("Cách xử lý vết thương khi bị chó cắn", R.drawable.blog2_image_6, "20.3.2024", "Hiện nay bệnh dại chưa có thuốc điều trị đặc hiệu. Xử lý vết thương khi bị chó cắn đúng cách và được tiêm vắc-xin...",2));
         blogAdapter = new BlogAdapter(blogs);
         binding.homeblog.setAdapter(blogAdapter);
-    }
-
-    private void initData() {
-        products = new ArrayList<>();
-        products.add(new Product(R.drawable.pate1, "Thức ăn mèo gâu gâu", 20000,3,3,32,20000));
-        products.add(new Product(R.drawable.pate1, "Thức ăn mèo gâu gâu", 20000,3,3, 32, 20000));
-        products.add(new Product(R.drawable.pate1, "Thức ăn mèo gâu gâu", 20000,3,3,20,30000));
-
-        adapter = new ProductAdapter(products);
-    }
-
-    private void loadData() {
-        binding.lvlHomeSale.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        binding.lvlHomeSale.setAdapter(adapter);
-        binding.lvlHomeSale.setHasFixedSize(true);
-        binding.lvlHomeProduct1.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        binding.lvlHomeProduct1.setAdapter(adapter);
-        binding.lvlHomeProduct1.setHasFixedSize(true);
-        binding.lvlHomeProduct2.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        binding.lvlHomeProduct2.setAdapter(adapter);
-        binding.lvlHomeProduct2.setHasFixedSize(true);
     }
 
     public void greeting(User user) {
