30942111ccd48ddce30943c38c524b768d4f3369
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-04-23 09:00:33 +0700

    Fix: button cate UI in product

--- app/src/main/java/com/unipet7/mcommerce/utils/LoadingDialog.java
+++ app/src/main/java/com/unipet7/mcommerce/utils/LoadingDialog.java
@@ -3,6 +3,7 @@ package com.unipet7.mcommerce.utils;
 import android.app.Dialog;
 import android.content.Context;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.unipet7.mcommerce.R;
 
 import java.util.Objects;
@@ -10,9 +11,12 @@ import java.util.Objects;
 public class LoadingDialog {
 
     Dialog dialog;
+    LottieAnimationView lvLoading;
     public void showLoadingDialog(Context context) {
         dialog = new Dialog(context);
         dialog.setContentView(R.layout.loading_dialog);
+        lvLoading = dialog.findViewById(R.id.lav_loading);
+        lvLoading.playAnimation();
         dialog.setCancelable(false);
         Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(android.R.color.transparent);
         dialog.show();
@@ -20,6 +24,7 @@ public class LoadingDialog {
 
     public void dissmis() {
         if (dialog != null) {
+            lvLoading.cancelAnimation();
             dialog.dismiss();
         }
     }
