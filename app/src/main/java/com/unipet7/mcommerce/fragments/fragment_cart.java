d6447df8b91e777d0f222a2f4eec6ab548e70363
Luan Nguyen <luannpd2k3@gmail.com>
2024-04-23 22:06:15 +0700

    pull code

--- app/src/main/java/com/unipet7/mcommerce/fragments/fragment_cart.java
+++ app/src/main/java/com/unipet7/mcommerce/fragments/fragment_cart.java
@@ -1,21 +1,33 @@
 package com.unipet7.mcommerce.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.TextAppearanceSpan;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,9 +42,14 @@ import com.unipet7.mcommerce.adapters.NotiAdapter;
 import com.unipet7.mcommerce.databinding.FragmentCartBinding;
 import com.unipet7.mcommerce.models.Notice;
 import com.unipet7.mcommerce.models.ProductCart;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import java.util.ArrayList;
-
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link fragment_cart#newInstance} factory method to
@@ -42,10 +59,80 @@ public class fragment_cart extends Fragment {
     FragmentCartBinding binding;
     ArrayList<ProductCart> productCarts;
     CartAdapter adapter;
+
     private static final int REQUEST_CODE_VOUCHER = 101;
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            if (direction == ItemTouchHelper.LEFT) {
+                // Xử lý khi vuốt sang trái
+            } else if (direction == ItemTouchHelper.RIGHT) {
+                // Xử lý khi vuốt sang phải
+            }
+        }
+        @Override
+        public void onChildDraw(
+                Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                float dX, float dY, int actionState, boolean isCurrentlyActive
+        ) {
+            View view = viewHolder.itemView;
+
+            Paint paint = new Paint();
+            paint.setColor(getResources().getColor(R.color.brandPrimary));
+            paint.setTextSize(30f);
+            paint.setAntiAlias(true);
+
+            // Fix position for button
+            float deleteButtonLeft = view.getRight() - (view.getRight() / 5f);
+            float deleteButtonTop = view.getTop();
+            float deleteButtonRight = view.getRight() - view.getPaddingRight();
+            float deleteButtonBottom = view.getBottom();
+
+            // Draw a button
+            float radius = 20f;
+            RectF deleteButtonDelete = new RectF(deleteButtonLeft, deleteButtonTop, deleteButtonRight, deleteButtonBottom);
+            c.drawRoundRect(deleteButtonDelete, radius, radius, paint);
+
+            // Set color for draw text inside button
+            paint.setColor(getResources().getColor(R.color.white));
+
+            // Load the vector drawable
+            Drawable deleteIconDrawable = AppCompatResources.getDrawable(getContext(), R.drawable.ic_cart_delete); // Replace R.drawable.ic_cart_delete with your actual vector icon
+
+// Set the bounds for the drawable
+            int iconWidth = deleteIconDrawable.getIntrinsicWidth();
+            int iconHeight = deleteIconDrawable.getIntrinsicHeight();
+            int iconLeft = (int) (deleteButtonDelete.centerX() - iconWidth / 2f);
+            int iconTop = (int) (deleteButtonDelete.centerY() - iconHeight / 2f);
+            deleteIconDrawable.setBounds(iconLeft, iconTop, iconLeft + iconWidth, iconTop + iconHeight);
+
+// Draw the vector icon
+            deleteIconDrawable.draw(c);
 
 
-    // TODO: Rename parameter arguments, choose names that match
+            // dX of item run from 0 to `-X` width of screen
+            // Item will stop in dX / 5
+            super.onChildDraw(c, recyclerView, viewHolder, dX / 5f, dY, actionState, isCurrentlyActive);
+        }
+
+        // Swipe back (start, end, top, down)
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            return makeMovementFlags(0, ItemTouchHelper.START);
+        }};
+
+
+
+
+
+
+        // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -84,13 +171,16 @@ public class fragment_cart extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+    ItemTouchHelper itemTouchHelper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentCartBinding.inflate(inflater, container, false);
-        setActionBar(binding.toolbar);
+        setActionBar(binding.toolbarall);
+        itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(binding.rcCart);
         binding.btnCircleVoucher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -140,30 +230,12 @@ public class fragment_cart extends Fragment {
 
 
     public void setActionBar(@Nullable Toolbar toolbar) {
-        AppCompatActivity activity = (AppCompatActivity) getActivity();
-        if (activity != null) {
-            activity.setSupportActionBar(toolbar);
-            ActionBar actionBar = activity.getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.setDisplayHomeAsUpEnabled(true);
-                actionBar.setHomeAsUpIndicator(R.drawable.next_btn_intro_white);
-                actionBar.setDisplayShowTitleEnabled(false);
-                actionBar.setDisplayShowCustomEnabled(true);
-
-                // Create a TextView programmatically
-                TextView textView = new TextView(activity);
-                textView.setText("Giỏ hàng");
-                textView.setTextColor(Color.BLACK);
-                textView.setGravity(Gravity.CENTER_HORIZONTAL);
-                SpannableString spannableString = new SpannableString(textView.getText());
-                spannableString.setSpan(new TextAppearanceSpan(activity, R.style.HEAD5_BOLD_18), 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-                textView.setText(spannableString);
-                Toolbar.LayoutParams layoutParams = new Toolbar.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT, Toolbar.LayoutParams.WRAP_CONTENT);
-                layoutParams.gravity = Gravity.CENTER;
-                textView.setLayoutParams(layoutParams);
-                actionBar.setCustomView(textView);
-            }
-        }
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_back_profile);
+        actionBar.setDisplayShowTitleEnabled(false);
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -175,6 +247,5 @@ public class fragment_cart extends Fragment {
             binding.edtVoucher.setText(voucherCode);
         }
     }
-
 
 }
