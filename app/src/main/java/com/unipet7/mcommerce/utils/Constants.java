54ebed9433401b035ccbe59242b6f80a49e1ea10
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-05-04 14:50:10 +0700

    UI: DetailProduct navigate to Cart

--- app/src/main/java/com/unipet7/mcommerce/utils/Constants.java
+++ app/src/main/java/com/unipet7/mcommerce/utils/Constants.java
@@ -24,4 +24,5 @@ public class Constants {
     public static final String FAVPRODUCTID = "favProductId";
     public static final String CATEGORYID = "categoryId";
     public static final String SALESPERCENT = "salepercent";
+    public static final String CART = "cart";
 }
