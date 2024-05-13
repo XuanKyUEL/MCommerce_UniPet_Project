3636f6ddc469ddcd6d211fa30d239367ed8d2486
duyennb2003 <duyennb21411@st.uel.edu.vn>
2024-05-13 20:28:42 +0700

    Change and Delete unused file

--- app/src/main/java/com/unipet7/mcommerce/fragments/HistoryOrder_Received.java
+++ app/src/main/java/com/unipet7/mcommerce/fragments/HistoryOrder_Received.java
@@ -84,8 +84,6 @@ public class HistoryOrder_Received extends Fragment {
 
     private List<HistoryOrders> initData() {
         Historyorders = new ArrayList<>();
-        Historyorders.add(new HistoryOrders("11/12/2024","1111", "Đã nhận", R.drawable.unipet_app_icon,"Royal Canin Rottweiler Puppy con mèo con đi hia long nhong",
-                "Túi", 1.0,24000.0, 24000.0));
 
         return Historyorders;
     }
